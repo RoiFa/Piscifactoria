fb2d62908a4862dcff9e47456d1d5b318f537e57
Roi Fandiño Alfonsín <royfandinoa@gmail.com>
2024-10-03 17:53:26 +0200

    Tanque acabado

--- src/tanque/Tanque.java
+++ src/tanque/Tanque.java
@@ -67,18 +67,50 @@ public abstract class Tanque {
         System.out.println("Tanque "+numTanque+" de la piscifactoría "+nombrePiscifactoria+" al "+(ocupacion()/maxSize)+"% de capacidad.["+ocupacion()+"/"+maxSize+"]");
     }
 
-    public void nextDay(int carne, int vegetal){
-        int carneCant = carne;
-        int vegetCant = vegetal;
+    public int[] nextDay(int carne, int vegetal){
         int[] cants;
         for(int i=0;i<peces.length;i++){
             if(peces[i]!=null){
-            cants = peces[i].grow(carneCant,vegetCant);
-            carneCant -= cants[0];
-            vegetCant -= cants[1];
+                if(!peces[i].isFertil()&&(peces[i].getEdad()-peces[i].getMadurez())%peces[i].getCiclo()==0){
+                    peces[i].setFertil(true);
+                }
+                cants = peces[i].grow(carne,vegetal);
+                carne -= cants[0];
+                vegetal -= cants[1];
+                if(peces[i].isVivo()&&!peces[i].isMale()&&peces[i].isFertil()){
+                    for(int k=0;k<peces[i].getHuevos();k++){
+                        Pez[] especiesMar = {new Rodaballo(),new Besugo(),new ArenqueDelAtlantico(),new Abadejo(),new Cobia(), new Dorada(),new BagreDeCanal()};
+                        Pez[] especiesRio = {new Carpa(),new Koi(),new SalmonChinook(),new TilapiaDelNilo(), new Pejerrey(), new Dorada(),new BagreDeCanal()};
+                        if(tipo=="Mar"){
+                            for(int j=0;j<especiesMar.length;j++){
+                                if(especiesMar[j].getNombre()==peces[0].getNombre()){
+                                    peces[findSpace()] = creadorEspeciesMar(j,true);
+                                }
+                            }
+                        }else{
+                            for(int j=0;j<especiesRio.length;j++){
+                                if(especiesRio[j].getNombre()==peces[0].getNombre()){
+                                    peces[findSpace()] = creadorEspeciesRio(j,true);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+        return new int[]{carne,vegetal};
     }
+
+    public boolean hayMacho(){
+        for(int i=0;i<peces.length;i++) {
+            if(peces[i].isMale()&&peces[i].isFertil()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
     public void addFish(){
         if(ocupacion()==maxSize){
@@ -86,10 +118,10 @@ public abstract class Tanque {
         }else if(ocupacion()==0){
             if(tipo=="Mar"){
                 menuEspeciesMar();
-                creadorEspeciesMar(Reader.readTheNumber());
+                creadorEspeciesMar(Reader.readTheNumber(),false);
             }else{
                 menuEspeciesRio();
-                creadorEspeciesRio(Reader.readTheNumber());
+                creadorEspeciesRio(Reader.readTheNumber(),false);
             }
         }else{
             System.out.println("Quiere añadir un "+peces[0].getNombre()+" mas al tanque?"+"\n"+"1.Si"+"\n"+"2.No");
@@ -100,13 +132,13 @@ public abstract class Tanque {
                 if(tipo=="Mar"){
                     for(int i=0;i<especiesMar.length;i++){
                         if(especiesMar[i].getNombre()==peces[0].getNombre()){
-                            peces[findSpace()] = creadorEspeciesMar(i);
+                            peces[findSpace()] = creadorEspeciesMar(i,false);
                         }
                     }
                 }else{
                     for(int i=0;i<especiesRio.length;i++){
                         if(especiesRio[i].getNombre()==peces[0].getNombre()){
-                            peces[findSpace()] = creadorEspeciesRio(i);
+                            peces[findSpace()] = creadorEspeciesRio(i,false);
                         }
                     }
                 }
@@ -154,7 +186,7 @@ public abstract class Tanque {
         return result;
     }
 
-    public Pez creadorEspeciesRio(int opcion){
+    public Pez creadorEspeciesRio(int opcion,boolean enReproduccion){
         switch (opcion) {
             default:
                 return null;
@@ -162,7 +194,7 @@ public abstract class Tanque {
                 System.out.println("Cancelando...");
                 break;
             case 1:
-                    if(ocupacion()==1){
+                    if(ocupacion()==1||enReproduccion){
                         if(peces[0].isMale()){
                             return new Carpa(false);
                         }else {
@@ -173,8 +205,8 @@ public abstract class Tanque {
                     }
 
             case 2:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Koi(false);
                     }else {
                         return new Koi(true);
@@ -184,8 +216,8 @@ public abstract class Tanque {
                 }
 
             case 3:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new SalmonChinook(false);
                     }else {
                         return new SalmonChinook(true);
@@ -195,8 +227,8 @@ public abstract class Tanque {
                 }
 
             case 4:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new TilapiaDelNilo(false);
                     }else {
                         return new TilapiaDelNilo(true);
@@ -206,8 +238,8 @@ public abstract class Tanque {
                 }
     
             case 5:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Pejerrey(false);
                     }else {
                         return new Pejerrey(true);
@@ -217,8 +249,8 @@ public abstract class Tanque {
                 }
 
             case 6:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Dorada(false);
                     }else {
                         return new Dorada(true);
@@ -228,8 +260,8 @@ public abstract class Tanque {
                 }
 
             case 7:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new BagreDeCanal(false);
                     }else {
                         return new BagreDeCanal(true);
@@ -241,7 +273,7 @@ public abstract class Tanque {
                 return null;
     }
 
-    public Pez creadorEspeciesMar(int opcion){
+    public Pez creadorEspeciesMar(int opcion,boolean enReproduccion){
         switch (opcion) {
             default:
                 return null;
@@ -249,8 +281,8 @@ public abstract class Tanque {
             System.out.println("Cancelando...");
                 break;
                 case 1:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Rodaballo(false);
                     }else {
                         return new Rodaballo(true);
@@ -260,8 +292,8 @@ public abstract class Tanque {
                 }
 
             case 2:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Besugo(false);
                     }else {
                         return new Besugo(true);
@@ -271,8 +303,8 @@ public abstract class Tanque {
                 }
 
             case 3:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new ArenqueDelAtlantico(false);
                     }else {
                         return new ArenqueDelAtlantico(true);
@@ -282,8 +314,8 @@ public abstract class Tanque {
                 }
 
             case 4:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Abadejo(false);
                     }else {
                         return new Abadejo(true);
@@ -293,8 +325,8 @@ public abstract class Tanque {
                 }
 
             case 5:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Cobia(false);
                     }else {
                         return new Cobia(true);
@@ -304,8 +336,8 @@ public abstract class Tanque {
                 }
 
                 case 6:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new Dorada(false);
                     }else {
                         return new Dorada(true);
@@ -315,8 +347,8 @@ public abstract class Tanque {
                 }
 
             case 7:
-                if(ocupacion()==1){
-                    if(peces[0].isMale()){
+                if(ocupacion()==1||enReproduccion){
+                    if(predominan()){
                         return new BagreDeCanal(false);
                     }else {
                         return new BagreDeCanal(true);
@@ -327,7 +359,25 @@ public abstract class Tanque {
         }
         return null;
     }
-            
+     
+    public boolean predominan(){
+        int m=0;
+        int w=0;
+        for(int i=0;i<peces.length;i++){
+            if(peces[i]!=null){
+                if(peces[i].isMale()){
+                    m++;
+                }else{
+                    w++;
+                }
+            }
+        }
+        if(m<w){
+            return false;
+        }else {
+            return true;
+        }
+    }
 
     public int ocupacion(){
         int count=0;
