1911e9730cc2c2c96fb2d1ecbf92269ef1e1704a
Roi <royfandinoa@gmail.com>
2024-12-03 18:07:55 +0100

    changes1

--- src/tanque/Tanque.java
+++ src/tanque/Tanque.java
@@ -2,7 +2,6 @@ package tanque;
 import helpers.ErrorWriter;
 import helpers.RNG;
 import helpers.Reader;
-import helpers.TranscriptWriter;
 import main.Almacen;
 import main.Simulador;
 import monedas.Monedas;
